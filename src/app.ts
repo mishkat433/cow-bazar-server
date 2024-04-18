b8341a561e882881d243a7d031b39fc90a344cd2
Mishkat433 <mishkat433@gmail.com>
2024-04-19 02:17:44 +0600

    initial server setup and typescript eslint husky setup

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express, { NextFunction, Request, Response } from 'express';
+const app = express();
+import cors from "cors"
+import morgan from 'morgan';
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get("/", async (req: Request, res: Response) => {
+    res.send("'Cow Bazar' server is available")
+})
+
+app.use(morgan('dev'))
+
+// app.use('/api/v1', router)
+
+// app.use(globalErrorHandler)
+
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+        success: false,
+        message: 'Not Found',
+        errorMessage: [{
+            path: req.originalUrl,
+            message: 'API not found'
+        }]
+    })
+    next()
+})
+
+
+export default app;
---
+++ src/config/routeManage.ts
@@ -0,0 +1,20 @@
+// import express from 'express';
+// import { UserRoutes } from '../routes/userRoutes';
+// import { authRoutes } from '../routes/authRoutes';
+
+// const router = express.Router();
+
+// const moduleRoutes = [
+//     {
+//         path: '/users',
+//         route: UserRoutes,
+//     },
+//     {
+//         path: '/auth',
+//         route: authRoutes,
+//     }
+// ];
+
+// moduleRoutes.forEach(routes => router.use(routes.path, routes.route));
+
+// export default router;
